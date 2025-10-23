776feee58c8f372aa5b63582b2608594e847a4c2
mani-miriyala28 <111078616+mani-miriyala28@users.noreply.github.com>
2025-10-23 08:46:41 +0530

    SIB

--- src/SIB/Car.java
+++ src/SIB/Car.java
@@ -1,4 +1,40 @@
 package SIB;
 
 public class Car {
+    String carName;
+    String carModel;
+    int carPrice;
+    static final int NO_Of_WHEELS;
+
+    static {
+        System.out.println("SIB Block console");
+    }
+
+    static{
+        NO_Of_WHEELS=4;
+        System.out.println("SIB Block");
+    }
+
+    public Car(){
+        System.out.println("Default Constructor");
+    }
+
+    public Car(String carName, String carModel, int carPrice){
+        this.carName=carName;
+        this.carModel=carModel;
+        this.carPrice=carPrice;
+        System.out.println("Parameterized Constructor");
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public int getCarPrice() {
+        return carPrice;
+    }
 }
--- src/SIB/CarMain.java
+++ src/SIB/CarMain.java
@@ -1,4 +1,16 @@
 package SIB;
 
 public class CarMain {
+
+    static{
+        System.out.println("Static Block from same class");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Method Invocation started");
+        Car car = new Car();
+        Car car1=new Car("Toyota","Camry",200000);
+
+        System.out.println("car1 Details:"+car1.getCarModel());
+
+    }
 }
